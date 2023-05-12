3a097b4c992de00ffb34c20ee98f2c9d14d24021
Katie Liu <katieliu59@gmail.com>
2023-05-12 14:15:41 -0400

    Implement uuid for each mock user and to post request

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require('uuid');
 const express = require("express");
 const app = express();
 
@@ -11,7 +12,8 @@ app.listen(PORT, () => {
 
 /* ---------------- Define data model ---------------- */
 let users = [
-	{
+  {
+    id: uuidv4(),
 		firstName: "Peter",
 		lastnName: "Parker",
 		dateOfBirth: "August 10, 2001",
@@ -19,7 +21,8 @@ let users = [
 			"https://coolhdwall.com/storage1/202107/peter-parker-spider-man-far-from-home-tom-holland-4k-phone-wallpaper-2160x3840.jpg",
 		bio: "Your friendly neighbourhood Spiderman. With great power comes great responsibility.",
 	},
-	{
+  {
+    id: uuidv4(),
 		firstName: "Tony",
 		lastnName: "Stark",
 		dateOfBirth: "May 29, 1970",
@@ -27,7 +30,8 @@ let users = [
 			"https://venturecafephiladelphia.org/wp-content/uploads/sites/11/2021/05/dace9a3da47d7d748e13af43f96344a4449c7688_original.jpeg",
 		bio: "There's one thing you can never take away from me: I am Iron Man.",
 	},
-	{
+  {
+    id: uuidv4(),
 		firstName: "Natasha",
 		lastnName: "Romanoff",
 		dateOfBirth: "December 3, 1984",
@@ -50,7 +54,7 @@ app.get("/users", (req, res) => {
 /* Add new user */
 app.post("/users", (req, res) => {
 	// Extract the user data from the request body
-	const newUser = req.body;
+  const newUser = {id: uuidv4(), ...req.body};
 
 	// Add the new user to the list of users
 	users.push(newUser);
@@ -60,5 +64,15 @@ app.post("/users", (req, res) => {
 });
 
 /* Remove user */
+app.delete("/users/:id", (req, res) => {
+	// Get the user ID from the request parameters
+	const userId = req.params.id;
+
+	// Remove the user from the users array by filtering out the user with a matching ID
+	users = users.filter((user) => user.id !== userId);
+
+	// Send a 204 No Content response indicating that the user was successfully deleted
+	res.sendStatus(204);
+});
 
 /* Update user */
