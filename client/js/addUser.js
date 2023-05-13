b6f4916931668dc8234b69ea7f6bcfbea64a7779
Katie Liu <katieliu59@gmail.com>
2023-05-12 23:43:13 -0400

    Create js file to contain add user modal logic

---
+++ client/js/addUser.js
@@ -0,0 +1,7 @@
+// Get references to the month, day, and year dropdown menus
+const monthDropdown = document.querySelector('#month');
+const dayDropdown = document.querySelector('#day');
+const yearDropdown = document.querySelector('#year');
+
+// Create arrays for the months
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
