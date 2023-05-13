bcad1796b64965da9761a5bfdece2f95c3d109a8
Katie Liu <katieliu59@gmail.com>
2023-05-13 00:31:43 -0400

    Create submit post request to server

--- client/js/addUser.js
+++ client/js/addUser.js
@@ -1,34 +1,70 @@
+/* ---------------- Date of birth dropdown ---------------- */
+
 // Get references to the month, day, and year dropdown menus
-const monthDropdown = document.querySelector('#month');
-const dayDropdown = document.querySelector('#day');
-const yearDropdown = document.querySelector('#year');
+const monthDropdown = document.querySelector("#month");
+const dayDropdown = document.querySelector("#day");
+const yearDropdown = document.querySelector("#year");
 
 // Create arrays for the months, days, years
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
 const days = Array.from({ length: 31 }, (_, index) => index + 1);
 const currentYear = new Date().getFullYear();
 const years = Array.from({ length: 50 }, (_, index) => currentYear - index);
 
 // Populate the options for the month dropdown
 months.forEach((month, index) => {
-    const option = document.createElement('option');
-    option.text = month;
-    option.value = index + 1;
-    monthDropdown.add(option);
+	const option = document.createElement("option");
+	option.text = month;
+	option.value = index + 1;
+	monthDropdown.add(option);
 });
 
 // Populate the options for the day dropdown
 days.forEach((day) => {
-    const option = document.createElement('option');
-    option.text = day;
-    option.value = day;
-    dayDropdown.add(option);
-})
+	const option = document.createElement("option");
+	option.text = day;
+	option.value = day;
+	dayDropdown.add(option);
+});
 
 // Populate the options for the year dropdown
 years.forEach((year) => {
-    const option = document.createElement('option');
-    option.text = year;
-    option.value = year;
-    yearDropdown.add(option);
-  });
+	const option = document.createElement("option");
+	option.text = year;
+	option.value = year;
+	yearDropdown.add(option);
+});
+
+/* ---------------- Submit form data to server ---------------- */
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    // Get form data
+    const formData = new FormData(form);
+
+    // Convert form data into object
+    const userData = {};
+    formData.forEach((value, key) => {
+        userData[key] = value;
+    })
+
+    // Send POST request to backend server to add new user
+    
+
+});
