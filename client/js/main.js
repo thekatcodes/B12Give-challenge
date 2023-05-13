5eea4d9b0be2d1bdd36fb50186a7a3109fa9d6f2
Katie Liu <katieliu59@gmail.com>
2023-05-13 14:12:51 -0400

    Wrap fetch in function to export

--- client/js/main.js
+++ client/js/main.js
@@ -1,19 +1,24 @@
 const userList = document.querySelector("main");
 
 // GET request to the server to fetch the list of users
-fetch("http://localhost:3000/users")
-  .then((response) => response.json())
-  .then(data => {
-    data.map(user => {
-      const userCard = document.createElement('div');
-      userCard.innerHTML = `
-        <div class="card">
-          <img src="${user.profilePicture}" alt="Profile Picture">
-          <h2>${user.firstName} ${user.lastName}</h2>
-        </div>
-      `;
-      userList.appendChild(userCard);
-    })
-  })
-    .catch(error => console.error(error));
-  
+export function renderUsers() {
+	// Fetch list of users from the server
+	fetch("http://localhost:3000/users")
+		.then((response) => response.json())
+		.then((data) => {
+			data.map((user) => {
+				const userCard = document.createElement("div");
+				userCard.innerHTML = `
+            <div class="card">
+              <img src="${user.profilePicture}" alt="Profile Picture">
+              <h2>${user.firstName} ${user.lastName}</h2>
+            </div>
+          `;
+				userList.appendChild(userCard);
+			});
+		})
+		.catch((error) => console.error(error));
+}
+
+// Call function to display users right away 
+renderUsers();
