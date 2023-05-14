0f380e631302719ce9eefe30c621e9f00803443f
Katie Liu <katieliu59@gmail.com>
2023-05-14 03:51:14 -0400

    Move birthdate dropdowns logic to helper file

---
+++ client/js/helper.js
@@ -0,0 +1,47 @@
+function dobDropdown(monthDropdown, dayDropdown, yearDropdown) {
+ 
+    // Create arrays for the months, days, years
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const days = Array.from({ length: 31 }, (_, index) => index + 1);
+    const currentYear = new Date().getFullYear();
+    const years = Array.from({ length: 50 }, (_, index) => currentYear - index);
+  
+    // Populate the options for the month dropdown
+    months.forEach((month) => {
+      const option = document.createElement("option");
+      option.text = month;
+      option.value = month;
+      monthDropdown.add(option);
+    });
+  
+    // Populate the options for the day dropdown
+    days.forEach((day) => {
+      const option = document.createElement("option");
+      option.text = day;
+      option.value = day;
+      dayDropdown.add(option);
+    });
+  
+    // Populate the options for the year dropdown
+    years.forEach((year) => {
+      const option = document.createElement("option");
+      option.text = year;
+      option.value = year;
+      yearDropdown.add(option);
+    });
+};
+
+export { dobDropdown };
