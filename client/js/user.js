032e12d20eb74299c05f08a8185f06156cc0c649
Katie Liu <katieliu59@gmail.com>
2023-05-14 03:00:42 -0400

    Create fetch request to retrieve specific user data by id

--- client/js/user.js
+++ client/js/user.js
@@ -7,3 +7,23 @@ const userId = decodeURIComponent(urlParams.get('id'));
 console.log('userId:', userId); // check the value of userId
 
 
+
+fetch(`http://localhost:3000/users/${userId}`)
+    .then((response) => response.json())
+    .then((user) => {
+
+        console.log(user)
+
+        const userProfile = document.querySelector('main');
+        
+        
+        const profileDetail = document.createElement('div');
+        profileDetail.innerHTML = `
+        <div>
+        <h1>${user.firstName}</h1>
+        </div>
+        `
+        userProfile.appendChild(profileDetail);
+
+    })
+    .catch((error) => console.error(error));
