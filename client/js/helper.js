c86540cb2be5b1cb16f388c063e4cf7618969a30
Katie Liu <katieliu59@gmail.com>
2023-05-14 04:04:18 -0400

    Move arrays outside function to enable export

--- client/js/helper.js
+++ client/js/helper.js
@@ -1,24 +1,24 @@
+// Create arrays for the months, days, years
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const days = Array.from({ length: 31 }, (_, index) => index + 1);
+const currentYear = new Date().getFullYear();
+const years = Array.from({ length: 50 }, (_, index) => currentYear - index);
+
 function dobDropdown(monthDropdown, dayDropdown, yearDropdown) {
  
-    // Create arrays for the months, days, years
-    const months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    const days = Array.from({ length: 31 }, (_, index) => index + 1);
-    const currentYear = new Date().getFullYear();
-    const years = Array.from({ length: 50 }, (_, index) => currentYear - index);
-  
     // Populate the options for the month dropdown
     months.forEach((month) => {
       const option = document.createElement("option");
@@ -44,4 +44,4 @@ function dobDropdown(monthDropdown, dayDropdown, yearDropdown) {
     });
 };
 
-export { dobDropdown };
+export { dobDropdown, years };
