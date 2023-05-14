48c8dd28c462633a0c69a465d6cc1cbdad397d71
Katie Liu <katieliu59@gmail.com>
2023-05-14 04:04:59 -0400

    Move date of birth dropdown logic out

--- client/js/addUser.js
+++ client/js/addUser.js
@@ -9,48 +9,8 @@ const monthDropdown = document.querySelector("#month");
 const dayDropdown = document.querySelector("#day");
 const yearDropdown = document.querySelector("#year");
 
-// Create arrays for the months, days, years
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December",
-];
-const days = Array.from({ length: 31 }, (_, index) => index + 1);
-const currentYear = new Date().getFullYear();
-const years = Array.from({ length: 50 }, (_, index) => currentYear - index);
+dobDropdown(monthDropdown, dayDropdown, yearDropdown);
 
-// Populate the options for the month dropdown
-months.forEach((month) => {
-	const option = document.createElement("option");
-	option.text = month;
-	option.value = month;
-	monthDropdown.add(option);
-});
-
-// Populate the options for the day dropdown
-days.forEach((day) => {
-	const option = document.createElement("option");
-	option.text = day;
-	option.value = day;
-	dayDropdown.add(option);
-});
-
-// Populate the options for the year dropdown
-years.forEach((year) => {
-	const option = document.createElement("option");
-	option.text = year;
-	option.value = year;
-	yearDropdown.add(option);
-});
 
 /* ---------------- Submit form data to server ---------------- */
 
