eb6f0a60482c083b186de1a8aa2424aaa03e0e06
Katie Liu <katieliu59@gmail.com>
2023-05-14 02:40:37 -0400

    Update and clean code to only pass user id as query param

--- client/js/main.js
+++ client/js/main.js
@@ -7,39 +7,33 @@ export function renderUsers() {
 		.then((response) => response.json())
 		.then((data) => {
 			data.map((user) => {
-        const userCard = document.createElement("div");
-        console.log('user', user)
+				const userCard = document.createElement("div");
+				console.log("user", user);
+
+				// Set the `innerHTML` of the user card element to a string containing the user data
 				userCard.innerHTML = `
-            <div class="card" id="${user.id}" data-user=${JSON.stringify(user)}>
+            <div class="card" id="${user.id}">
               <img src="${user.profilePicture}" alt="Profile Picture">
               <h2>${user.firstName} ${user.lastName}</h2>
               <button class="view-profile">View Profile</button>
             </div>
           `;
-				// userCard.dataset.user = JSON.stringify(user);
-        userList.appendChild(userCard);
-        
-        // Select "View Profile" buttons
-        const viewProfileButton = userCard.querySelector(".view-profile");
-        
-        // Attach event listener to each button
-        viewProfileButton.addEventListener("click", () => {
-            console.log('TEST CLICK')
-          
-            // Get the user data from the "data-user" attribute
-            const userData = JSON.parse(userCard.dataset.user);
-        
-            // Redirect to user.html and pass the user data as a query parameter
-            window.location.href = `user.html?id=${userData.id}`;
-          });
-      });
-    })
-    .catch((error) => console.error(error));
-      
+				userList.appendChild(userCard);
+
+				// Select "View Profile" buttons
+				const viewProfileButton = userCard.querySelector(".view-profile");
+
+				// Attach event listener to each button
+				viewProfileButton.addEventListener("click", () => {
+					console.log("TEST CLICK");
+
+					// Redirect to user.html and pass the user id as a query parameter
+					window.location.href = `user.html?id=${user.id}`;
+				});
+			});
+		})
+		.catch((error) => console.error(error));
 }
 
 // Call function to display users right away
 renderUsers();
-
-
-// data-user='{"id": ${user.id}, "firstName": ${user.firstName}, "lastName": ${user.lastName}, "profilePicture": ${user.profilePicture}, "bio": ${user.bio}}'
