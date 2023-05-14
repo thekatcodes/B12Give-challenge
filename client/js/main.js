c5d9f792fc1c704f4f86a056d13ae35d70a18e25
Katie Liu <katieliu59@gmail.com>
2023-05-14 01:27:16 -0400

    Create click event listener to view profile

--- client/js/main.js
+++ client/js/main.js
@@ -9,16 +9,38 @@ export function renderUsers() {
 			data.map((user) => {
 				const userCard = document.createElement("div");
 				userCard.innerHTML = `
-            <div class="card">
+            <div class="card" id="${user.id}">
               <img src="${user.profilePicture}" alt="Profile Picture">
               <h2>${user.firstName} ${user.lastName}</h2>
+              <button class="view-profile">View Profile</button>
             </div>
           `;
+				userCard.dataset.user = JSON.stringify(user);
 				userList.appendChild(userCard);
 			});
 		})
 		.catch((error) => console.error(error));
 }
 
-// Call function to display users right away 
+// Call function to display users right away
 renderUsers();
+
+// Select "View Profile" buttons
+const viewProfileButton = document.querySelectorAll(".view-profile");
+
+// Attach event listener to each button
+viewProfileButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log('TEST CLICK')
+		// Get the card element
+		const card = button.parentElement;
+
+		// Get the user data from the "data-user" attribute
+		const userData = JSON.parse(card.dataset.user);
+
+		// Redirect to user.html and pass the user data as a query parameter
+		window.location.href = `user.html?id=${userData.id}`;
+	});
+});
+
+// data-user='{"id": ${user.id}, "firstName": ${user.firstName}, "lastName": ${user.lastName}, "profilePicture": ${user.profilePicture}, "bio": ${user.bio}}'
