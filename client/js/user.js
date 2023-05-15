4990fd7e34f258a3e0edc34e1f6b08c039f77457
Katie Liu <katieliu59@gmail.com>
2023-05-15 01:07:02 -0400

    Implement fetch with delete request to server to delete user

--- client/js/user.js
+++ client/js/user.js
@@ -12,12 +12,14 @@ console.log("userDataString:", userDataString);
 const userId = decodeURIComponent(urlParams.get("id"));
 console.log("userId:", userId); // check the value of userId
 
+let userData;
+
 // Fetch the user with the given id
 fetch(`http://localhost:3000/users/${userId}`)
 	.then((response) => response.json())
 	.then((user) => {
 		// console.log(user)
-
+    userData = user;
 		const userProfile = document.querySelector("main");
 
 		const profileDetail = document.createElement("div");
@@ -55,4 +57,23 @@ backButton.addEventListener("click", () => {
 
 deleteButton.addEventListener("click", () => {
 	// Add code to handle the click event for the delete button
+
+ 	// Send delete request to server with user data
+   fetch(`http://localhost:3000/users/${userId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((response) => {
+      console.log(response);
+      if (response.ok) {
+        window.location.href = `index.html`;
+
+        console.log("deleted user, now back to home page");
+      }
+    })
+    .catch((error) => console.error(error));
+
 });
