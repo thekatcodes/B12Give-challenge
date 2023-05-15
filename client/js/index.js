80e0230fc76c91aa5df67b31e26a501f10e268ae
Katie Liu <katieliu59@gmail.com>
2023-05-15 14:14:08 -0400

    Update click events to include overlay display style

--- client/js/index.js
+++ client/js/index.js
@@ -9,8 +9,8 @@ export function renderUsers() {
 		.then((response) => response.json())
 		.then((data) => {
 			data.map((user) => {
-        const userCard = document.createElement("div");
-        userCard.classList.add("card");
+				const userCard = document.createElement("div");
+				userCard.classList.add("card");
 				console.log("user", user);
 
 				// Set the `innerHTML` of the user card element to a string containing the user data
@@ -43,7 +43,6 @@ export function renderUsers() {
 // Call function to display users right away
 renderUsers();
 
-
 /* ---------------- Hide/Display Modal on click events ---------------- */
 
 // Select Add User button
@@ -51,22 +50,31 @@ const addUserButton = document.querySelector(".add-user-button");
 
 // Select modal element
 const modal = document.getElementById("add-user-modal");
+const overlay = document.querySelector(".overlay");
 
 // Select close button inside the modal
 const closeButton = modal.querySelector(".close");
 
 // Add event listener to Add User button
 addUserButton.addEventListener("click", () => {
-  console.log('+ Add User clicked')
-  // Show the modal
-  modal.style.display = "block";
+	console.log("+ Add User clicked");
+	// Show the modal
+	modal.style.display = "block";
+	overlay.style.display = "block";
 });
-
 
 // Add event listener to close (x) button
 closeButton.addEventListener("click", () => {
-  console.log('close icon clicked')
+	console.log("close icon clicked");
 
-  // Hide the modal
-  modal.style.display = "none";
+	// Hide the modal
+	modal.style.display = "none";
+	overlay.style.display = "none";
+});
+
+// Add event listener to overlay
+overlay.addEventListener("click", () => {
+	// Hide the modal and overlay
+	modal.style.display = "none";
+	overlay.style.display = "none";
 });
